68a0ccec0d26dad524482079974df567476d6dbf
Senthil_03 <115228001+SenthilMurugan-03@users.noreply.github.com>
2023-07-26 15:44:39 +0530

    Half-Subtractor-TB.v

---
+++ VERILOG_PROGRAM_DIGITAL_CIRCUITS/Half_Subtractor/Half-Subtractor-TB.v
@@ -0,0 +1,12 @@
+module Half_Sub_TB;
+reg A,B;
+wire D,B;
+Half_Sub(A,B,D,B);
+initial
+begin
+#10 A = 1'b0 ; B = 1'b0;
+#10 A = 1'b0 ; B = 1'b1;
+#10 A = 1'b1 ; B = 1'b0;
+#10 A = 1'b1 ; B = 1'b1;
+end
+endmodule
