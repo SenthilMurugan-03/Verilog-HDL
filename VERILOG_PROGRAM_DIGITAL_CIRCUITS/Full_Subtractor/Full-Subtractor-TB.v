18aa8c590508fd47bfc1bad8a465203123c6a38e
Senthil_03 <115228001+SenthilMurugan-03@users.noreply.github.com>
2023-07-26 15:51:59 +0530

    Update Full-Subtractor-TB.v

--- VERILOG_PROGRAM_DIGITAL_CIRCUITS/Full_Subtractor/Full-Subtractor-TB.v
+++ VERILOG_PROGRAM_DIGITAL_CIRCUITS/Full_Subtractor/Full-Subtractor-TB.v
@@ -5,15 +5,15 @@ Full_Subtractor(A,B,C_IN,D,B);
 initial 
 begin
 
-#10 A = 0; B = 0; C_IN = 0;
-#10 A = 0; B = 0; C_IN = 1;
-#10 A = 0; B = 1; C_IN = 0;
-#10 A = 0; B = 1; C_IN = 1;
-#10 A = 1; B = 0; C_IN = 0;
-#10 A = 1; B = 0; C_IN = 1;
-#10 A = 1; B = 1; C_IN = 0;
-#10 A = 1; B = 1; C_IN = 1;
+#10 A = 1'b0; B = 1'b0; C_IN = 1'b0;
+#10 A = 1'b0; B = 1'b0; C_IN = 1'b1;
+#10 A = 1'b0; B = 1'b1; C_IN = 1'b0;
+#10 A = 1'b0; B = 1'b1; C_IN = 1'b1;
+#10 A = 1'b1; B = 1'b0; C_IN = 1'b0;
+#10 A = 1'b1; B = 1'b0; C_IN = 1'b1;
+#10 A = 1'b1; B = 1'b1; C_IN = 1'b0;
+#10 A = 1'b1; B = 1'b1; C_IN = 1'b1;
 
-#10 finish;
+#10 $finish;
 end
 endmodule
