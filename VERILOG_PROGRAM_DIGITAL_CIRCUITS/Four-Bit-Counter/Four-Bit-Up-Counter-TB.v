9555b5d4294d92854c8fb10f63f7649aaeb3c995
Senthil_03 <115228001+SenthilMurugan-03@users.noreply.github.com>
2023-07-26 16:16:45 +0530

    Create Four-Bit-Up-Counter-TB.v

---
+++ VERILOG_PROGRAM_DIGITAL_CIRCUITS/Four-Bit-Counter/Four-Bit-Up-Counter-TB.v
@@ -0,0 +1,21 @@
+module Four_Bit_UP_Counter_TB;
+
+reg clk,r;
+wire [3:0]count;
+
+module Four_Bit_UP_Counter(input clk, r, output[3:0] count);
+
+initial 
+begin
+
+#10 clk = 1; r = 0;
+#10 clk = 1; r = 0;
+#10 clk = 1; r = 0;
+#10 clk = 1; r = 0;
+#10 clk = 1; r = 1;
+
+#10 $finish;
+end 
+endmodule
+
+
