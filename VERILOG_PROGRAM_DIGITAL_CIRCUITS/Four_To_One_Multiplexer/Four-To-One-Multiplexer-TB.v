56ce05b87da831a16acd061f996cccf09656483e
Senthil_03 <115228001+SenthilMurugan-03@users.noreply.github.com>
2023-07-26 15:48:50 +0530

    Update Four-To-One-Multiplexer-TB.v

--- VERILOG_PROGRAM_DIGITAL_CIRCUITS/Four_To_One_Multiplexer/Four-To-One-Multiplexer-TB.v
+++ VERILOG_PROGRAM_DIGITAL_CIRCUITS/Four_To_One_Multiplexer/Four-To-One-Multiplexer-TB.v
@@ -5,9 +5,9 @@ Four_To_One_Mux([0:3]A,S0,S1,OUT);
 initial
 begin
 A = {1001};
-#10 S0 = 0; S1 = 0;
-#10 S0 = 0; S1 = 1;
-#10 S0 = 1; S1 = 0;
-#10 S0 = 1; S1 = 1;
+#10 S0 = 1'b0; S1 = 1'b0;
+#10 S0 = 1'b0; S1 = 1'b1;
+#10 S0 = 1'b1; S1 = 1'b0;
+#10 S0 = 1'b1; S1 = 1'b1;
 end
 endmodule
