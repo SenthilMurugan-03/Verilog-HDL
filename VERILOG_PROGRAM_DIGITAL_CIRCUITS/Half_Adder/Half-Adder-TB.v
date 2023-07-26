f1aacb42cefb96cc5873adb5f84b5c39b08aeb2b
Senthil_03 <115228001+SenthilMurugan-03@users.noreply.github.com>
2023-07-26 15:53:34 +0530

    Update Half-Adder-TB.v

--- VERILOG_PROGRAM_DIGITAL_CIRCUITS/Half_Adder/Half-Adder-TB.v
+++ VERILOG_PROGRAM_DIGITAL_CIRCUITS/Half_Adder/Half-Adder-TB.v
@@ -5,9 +5,9 @@ Half_Add uut(A,B,S,C);
 initial
 begin
 A = 0; B = 0;
-#10 A = 0; B = 1;
-#10 A = 1; B = 0;
-#10 A = 1; B = 1;
+#10 A = 1'b0; B = 1'b1;
+#10 A = 1'b1; B = 1'b0;
+#10 A = 1'b1; B = 1'b1;
 #10 $finish;
 end
 endmodule
